3f2c7f85e95e400f9975f15a3e693e1c2a358869
kritisharma206 <55374953+kritisharma206@users.noreply.github.com>
2019-10-14 23:55:16 +0530

    Update s1.java

--- String/s1.java
+++ String/s1.java
@@ -1,13 +1,14 @@
+
 public class s1{
     public static void main(String args[])
     {
         //false answer as both as different obj as we use new memory make new object
         // but if we use s1.equals(s2) its true
         // also in " " here we have true false it also creates 2 objects " " creates string objects
-        String s1=new String("abc");
+        String s1=new String("abc"); //string declare
         String s2=new String("abc");
 
-        if(s1==s2)
+        if(s1==s2) //comparison
         {
             System.out.println("True");
         }
